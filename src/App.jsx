3e794c5888484a0b8c1652c278048d7dc469ad16
renzo01 <renzogomez577@gmail.com>
2020-05-15 14:29:41 -0500

    Arreglo de Components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+//import registerFormSeller from './components/registerFormSellers/registerFormSeller';
+import LoginFormSeller from './components/login/loginFormSeller';
+function App() {
+  return (
+    <div className="App">
+      <h1>Pagina Principal</h1>
+      {/*<registerFormSeller/>*/}
+      <LoginFormSeller/>
+    </div>
+  );
+}
+
+export default App;
--- src/components/registerFormSellers/registerFormSeller.jsx
+++ src/components/registerFormSellers/registerFormSeller.jsx
@@ -3,7 +3,27 @@ import React from 'react'
 function registerFormSeller() {
     return (
         <div>
-            <h1>Hola desde registerFormSeller</h1>
+           <h3>Registro de vendedor</h3>
+      <form>
+        <label for="nombre">Nombre</label>
+        <input id="nombre" name="nombre" required />
+        <label for="correo">Correo</label>
+        <input id="Correo" name="Correo" type="email" required />
+        <label for="contrasenia">Contraseña</label>
+        <input id="contrasenia" name="contrasenia" type="password" required />
+        <label for="razonSocial">Razon Soc.</label>
+        <input id="razonSocial" name="razonSocial" required />
+        <label for="ruc">R.U.C</label>
+        <input id="ruc" name="ruc" required />
+        <label for="delivery">Delivery</label>
+        <input id="delivery" name="delivery" />
+        <label for="pagWeb">Pag. web</label>
+        <input id="pagWeb" name="pagWeb" />
+        <label for="contacto">Contacto</label>
+        <input id="contacto" name="contacto" />
+        <label for="logo">Logo</label>
+        <input id="logo" name="logo" type="file" />
+      </form>
         </div>
     )
 }
