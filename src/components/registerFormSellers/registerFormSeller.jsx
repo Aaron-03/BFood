e8b6912a8dba4c39471159fd3d437ce05ce148c6
renzo01 <renzogomez577@gmail.com>
2020-05-15 12:31:51 -0500

    Estructura del formulario Registro Vendedor

---
+++ src/components/registerFormSellers/registerFormSeller.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function registerFormSeller() {
+    return (
+        <div>
+            <h1>Hola desde registerFormSeller</h1>
+        </div>
+    )
+}
+
+export default registerFormSeller;
