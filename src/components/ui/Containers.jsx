d34b9027e5a4f717f4087f6d449187b2331915b6
renzo01 <renzogomez577@gmail.com>
2020-05-21 15:35:07 -0500

    new General Container

--- src/components/ui/Containers.jsx
+++ src/components/ui/Containers.jsx
@@ -1,11 +1,19 @@
 import styled from '@emotion/styled';
 
 export const ContainerGeneralSeller = styled.div`
-    height: 100vh;
-    width: 100vw;
-    background-color: var(--custom-blue);
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: var(--custom-blue);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ContainerRegistroProducto = styled.div`
+  background-color: var(--custom-blue);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
