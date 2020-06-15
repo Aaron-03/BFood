858101423a00b042ea1016c8384a1dd62c1de541
renzo01 <renzogomez577@gmail.com>
2020-06-15 17:36:53 -0500

    Sucursales Update 1

--- src/context/products/ProductReducer.jsx
+++ src/context/products/ProductReducer.jsx
@@ -1,27 +1,15 @@
 import ProductTypes from '../../types/ProductTypes';
 
 const {
-<<<<<<< HEAD
   ADD_PRODUCT,
   UPD_PRODUCT,
   DLT_PRODUCT,
-  LST_PRODUCT,
+  LST_PRODUCT_BY_PAGE,
   LST_PRODUCT_BY_TERM,
   FAIL_PRODUCT,
   RESET_PRODUCT,
   FILTER_PRODUCT_BY_CATEGORY,
   FILTER_PRODUCT_BY_PRICE,
-=======
-    ADD_PRODUCT,
-    UPD_PRODUCT,
-    DLT_PRODUCT,
-    LST_PRODUCT_BY_PAGE,
-    LST_PRODUCT_BY_TERM,
-    FAIL_PRODUCT,
-    RESET_PRODUCT,
-    FILTER_PRODUCT_BY_CATEGORY,
-    FILTER_PRODUCT_BY_PRICE
->>>>>>> 10ee00ffedbefcb6560c5dc84a09d57decdf4c43
 } = ProductTypes;
 
 export default (state, action) => {
@@ -32,19 +20,13 @@ export default (state, action) => {
       };
 
     case UPD_PRODUCT:
-<<<<<<< HEAD
       return {};
-=======
-      return {
 
-      };
-    
     case LST_PRODUCT_BY_PAGE:
       return {
         ...state,
-        products: action.payload
+        products: action.payload,
       };
->>>>>>> 10ee00ffedbefcb6560c5dc84a09d57decdf4c43
 
     case LST_PRODUCT_BY_TERM:
       return {
